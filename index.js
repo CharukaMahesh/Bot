afed8d26ac111d6e02e1f81e0a4f4ed1f9a0c793
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-11-10 16:22:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -83,10 +83,7 @@ if (!mek.message) return
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true") {
     // Mark the status message as read
-    await conn.readMessages([mek.key]);
-
-    // React to the status message with 🌝 emoji
-    await conn.sendMessage(mek.key.remoteJid, { react: { text: '🌝', key: mek.key } });
+    await conn.readMessages([mek.key])
 }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
